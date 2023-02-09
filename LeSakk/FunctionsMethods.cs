932c2d54cf84b987bbf65dde48d675a12ab18f83
ginalzsolt <ginalzsolt@turr.hu>
2023-02-09 09:38:44 +0100

    c:

--- LeSakk/FunctionsMethods.cs
+++ LeSakk/FunctionsMethods.cs
@@ -8,5 +8,27 @@ namespace LeSakk
 {
     class FunctionsMethods
     {
+        public static void gamelogic()
+        {
+
+        }
+        public static void Swap()
+        {
+            Data.isWhite = !Data.isWhite;
+        }
+        public static void PesantToQween()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                if (Data.Field[0,i].Type==1)
+                {
+                    AlterPiece(0,i);
+                }
+            }
+        }
+        public static void AlterPiece( int y ,int x )
+        {
+            new PieceChoser(y,x).Show();
+        }
     }
 }
